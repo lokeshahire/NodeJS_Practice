ec6568d84fe14070fe16c37bd3f5b80222fab42a
Lokesh <iamlokeshahire@gmail.com>
2023-01-08 20:06:20 +0530

    auth practice

--- Authentication/configs/db.js
+++ Authentication/configs/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect()
+const connection = mongoose.connect("mongodb+srv://lokesh:ahire@cluster0.entjnlc.mongodb.net/auth?retryWrites=true&w=majority")
