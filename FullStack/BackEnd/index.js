ec56c87112f4fb195b2195aa867bcb6d52054a9f
Lokesh <iamlokeshahire@gmail.com>
2023-01-09 20:11:23 +0530

    FullStack practice

--- FullStack/BackEnd/index.js
+++ FullStack/BackEnd/index.js
@@ -2,3 +2,5 @@ const express = require("express");
 const { connection } = require("./configs/db")
 const { userRouter } = require("./routes/User.routes")
 const { noteRouter } = require("./routes/Note.routes")
+const { authenticate } = require("./middleware/authenticate.middleware")
+const cors = require("cors")
