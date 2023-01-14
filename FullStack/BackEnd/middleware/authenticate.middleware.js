39db19b6e518857aa21a17be76c6ffbee338827c
Lokesh <iamlokeshahire@gmail.com>
2023-01-14 20:28:56 +0530

    FullStack practice

--- FullStack/BackEnd/middleware/authenticate.middleware.js
+++ FullStack/BackEnd/middleware/authenticate.middleware.js
@@ -5,7 +5,9 @@ const authenticate = (req, res, next) => {
     const token = req.headers.authorization
 
     if (token) {
+        jwt.verify(token, "masai", (err, decoded) => {
 
+        })
     }
     else {
         res.send({ msg: "Please Login" })
