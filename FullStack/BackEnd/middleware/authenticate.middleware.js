739c739262cb404e2830959202fb56766acbdf09
Lokesh <iamlokeshahire@gmail.com>
2023-01-14 20:28:04 +0530

    FullStack practice

--- FullStack/BackEnd/middleware/authenticate.middleware.js
+++ FullStack/BackEnd/middleware/authenticate.middleware.js
@@ -1,2 +1,14 @@
 const jwt = require("jsonwebtoken")
 
+const authenticate = (req, res, next) => {
+
+    const token = req.headers.authorization
+
+
+
+}
+
+
+module.exports = {
+    authenticate
+}
