5d67e7f62d988ca1b65838ebddc79dd089329958
Lokesh <iamlokeshahire@gmail.com>
2023-01-17 20:43:28 +0530

    FullStack practice

--- FullStack/BackEnd/routes/User.routes.js
+++ FullStack/BackEnd/routes/User.routes.js
@@ -6,10 +6,17 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt")
 
 userRouter.post("/register", async (req, res) => {
+    const { name, email, password } = req.body
+    try {
+        bcrypt.hash(password, 5, async (err, hash) => {
+            if (err) {
+                res.send({ msg: "user registration failed", "error": err.message });
+            }
 
 
 
-    module.exports = { userRouter }
+
+            module.exports = { userRouter }
 
 
 
