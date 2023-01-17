99e6a9d56da539910e55f22d5ae3cce3f4c9cb96
Lokesh <iamlokeshahire@gmail.com>
2023-01-17 20:42:24 +0530

    FullStack practice

--- FullStack/BackEnd/routes/User.routes.js
+++ FullStack/BackEnd/routes/User.routes.js
@@ -1,3 +1,7 @@
 
 const express = require('express');
 const { UserModel } = require("../model/User.model");
+const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
+
