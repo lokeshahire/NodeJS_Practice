1553a432467a445d7e7f654a35523efb3384db48
Lokesh <iamlokeshahire@gmail.com>
2023-03-01 19:49:57 +0530

    auth practice

--- Authentication/index.js
+++ Authentication/index.js
@@ -2,3 +2,10 @@ const express = require("express");
 const { connection } = require("./configs/db");
 const { UserModel } = require("./model/User.model");
 const jwt = require("jsonwebtoken")
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("HOME PAGE");
+});
